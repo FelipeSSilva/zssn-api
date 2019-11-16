b71298c0eec72121b2c6925bec43f1a531f036cf
Felipe Silva <felipesilva.devl@gmail.com>
2019-11-16 15:47:47 -0300

    Adicionando tryCatch às funções do controller

--- app/Http/Controllers/SurvivorsController.php
+++ app/Http/Controllers/SurvivorsController.php
@@ -16,241 +16,283 @@ class SurvivorsController extends Controller
 {
     public function index()
     {
-        $survivors = Survivor::get();
-        return response()->json($survivors);
+        try {
+            $survivors = Survivor::get();
+            return response()->json($survivors);
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
+        }
+
     }
 
     public function store(StoreSurvivorsRequest $request)
     {
-        $survivor = new Survivor();
-        $survivor->fill($request->all());
-        $survivor->save();
+        try {
+            $survivor = new Survivor();
+            $survivor->fill($request->all());
+            $survivor->save();
 
-        if ($request->resources) {
-            foreach ($request->resources as $itemName => $quantity) {
-                $item = Item::where('name', 'like', '%' . $itemName . '%')->first();
-                if (!empty($item->id)) {
-                    $resource = new Resource();
-                    $resource->item_id = $item->id;
-                    $resource->survivor_id = $survivor->id;
-                    $resource->quantity = $quantity;
+            if ($request->resources) {
+                foreach ($request->resources as $itemName => $quantity) {
+                    $item = Item::where('name', 'like', '%' . $itemName . '%')->first();
+                    if (!empty($item->id)) {
+                        $resource = new Resource();
+                        $resource->item_id = $item->id;
+                        $resource->survivor_id = $survivor->id;
+                        $resource->quantity = $quantity;
 
-                    $resource->save();
+                        $resource->save();
+                    }
                 }
             }
-        }
 
-        return response()->json($survivor, 201);
+            return response()->json($survivor, 201);
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
+        }
 
     }
 
     public function update(UpdateSurvivorsRequest $request, $id)
     {
-        $survivor = Survivor::find($id);
+        try {
+            $survivor = Survivor::find($id);
 
-        if (!$survivor) {
-            return response()->json([
-                'message' => 'Survivor not found',
-            ], 404);
+            if (!$survivor) {
+                return response()->json([
+                    'message' => 'Survivor not found',
+                ], 404);
+            }
+
+            $survivor->latitude = $request->latitude;
+            $survivor->longitude = $request->longitude;
+            $survivor->save();
+
+            return response()->json($survivor);
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
         }
-
-        $survivor->latitude = $request->latitude;
-        $survivor->longitude = $request->longitude;
-        $survivor->save();
-
-        return response()->json($survivor);
     }
 
     public function reportInfection($survivor_reporter_id, $survivor_infected_id)
     {
-        $survivorReporter = Survivor::find($survivor_reporter_id);
-        if (!$survivorReporter) {
-            return response()->json([
-                'message' => 'Survivor not found',
-            ], 404);
-        }
-
-        $survivorInfected = Survivor::find($survivor_infected_id);
-        if (!$survivorInfected) {
-            return response()->json([
-                'message' => 'Survivor not found',
-            ], 404);
-        }
-
-        $reportCreated = InfectionReport::where('survivor_reporter_id', $survivor_reporter_id)->where('survivor_infected_id',$survivor_infected_id)->first();
-        if($reportCreated){
-            return response()->json([
-                'message' => 'Report already created',
-            ], 400);
-        }
-
-
-        $infectedReport = new InfectionReport();
-        $infectedReport->survivor_reporter_id = $survivor_reporter_id;
-        $infectedReport->survivor_infected_id = $survivor_infected_id;
-        $infectedReport->save();
-
-        if ($survivorInfected->infectionReportsCount >= 3) {
-            if ($survivorInfected->infected != 'Y') {
-                $survivorInfected->infected = 'Y';
-                $survivorInfected->save();
+        try {
+            $survivorReporter = Survivor::find($survivor_reporter_id);
+            if (!$survivorReporter) {
+                return response()->json([
+                    'message' => 'Survivor not found',
+                ], 404);
             }
 
-            return response()->json([
-                'message' => 'Survivor infected!',
-            ], 201);
-        } else {
-            return response()->json([
-                'message' => 'Unconfirmed infection!',
-            ], 201);
+            $survivorInfected = Survivor::find($survivor_infected_id);
+            if (!$survivorInfected) {
+                return response()->json([
+                    'message' => 'Survivor not found',
+                ], 404);
+            }
+
+            $reportCreated = InfectionReport::where('survivor_reporter_id', $survivor_reporter_id)->where('survivor_infected_id', $survivor_infected_id)->first();
+            if ($reportCreated) {
+                return response()->json([
+                    'message' => 'Report already created',
+                ], 400);
+            }
+
+
+            $infectedReport = new InfectionReport();
+            $infectedReport->survivor_reporter_id = $survivor_reporter_id;
+            $infectedReport->survivor_infected_id = $survivor_infected_id;
+            $infectedReport->save();
+
+            if ($survivorInfected->infectionReportsCount >= 3) {
+                if ($survivorInfected->infected != 'Y') {
+                    $survivorInfected->infected = 'Y';
+                    $survivorInfected->save();
+                }
+
+                return response()->json([
+                    'message' => 'Survivor infected!',
+                ], 201);
+            } else {
+                return response()->json([
+                    'message' => 'Unconfirmed infection!',
+                ], 201);
+            }
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
         }
     }
 
     public function tradeItems(TradeSurvivorsRequest $request, $survivor_offer_id, $survivor_accept_id)
     {
-        $survivorOffer = Survivor::find($survivor_offer_id);
-        if (!$survivorOffer) {
-            return response()->json([
-                'message' => 'Survivor not found',
-            ], 404);
-        } elseif ($survivorOffer->infected == 'Y') {
-            return response()->json([
-                'message' => 'Survivor infected!',
-            ], 400);
-        }
-
-        $survivorAccept = Survivor::find($survivor_accept_id);
-        if (!$survivorAccept) {
-            return response()->json([
-                'message' => 'Survivor not found',
-            ], 404);
-        } elseif ($survivorAccept->infected == 'Y') {
-            return response()->json([
-                'message' => 'Survivor infected!',
-            ], 400);
-        }
-
-        $checkItemEquality = self::checkItemsEquality($request->resourceSurvivorOffer, $request->resourceSurvivorAccept);
-        if ($checkItemEquality == false) {
-            return response()->json([
-                'message' => 'Different points!',
-            ], 400);
-        }
-
-        $checkQuantityMinSurvivorOffer = self::checkQuantityMin($request->resourceSurvivorOffer, $survivorOffer);
-        if ($checkQuantityMinSurvivorOffer == false) {
-            return response()->json([
-                'message' => 'No resources available!',
-            ], 400);
-        }
-
-        $checkQuantityMinSurvivorAccept = self::checkQuantityMin($request->resourceSurvivorAccept, $survivorAccept);
-        if ($checkQuantityMinSurvivorAccept == false) {
-            return response()->json([
-                'message' => 'No resources available!',
-            ], 400);
-        }
-
-        foreach ($request->resourceSurvivorOffer as $itemName => $quantityTrade) {
-            $item = Item::where('name', 'like', '%' . $itemName . '%')->first();
-            $resourceSurvivorOffer = Resource::where('item_id','=',$item->id)->where('survivor_id','=',$survivor_offer_id)->first();
-            $resourceSurvivorOffer->quantity -= $quantityTrade;
-            $resourceSurvivorOffer->save();
-
-            $resourceSurvivorAccept = Resource::where('item_id','=',$item->id)->where('survivor_id','=',$survivor_accept_id)->first();
-
-            if(!empty($resourceSurvivorAccept)){
-                $resourceSurvivorAccept->quantity += $quantityTrade;
-
-                $resourceSurvivorAccept->save();
-            }else{
-                $resourceSurvivorAccept = new Resource();
-                $resourceSurvivorAccept->survivor_id = $survivor_accept_id;
-                $resourceSurvivorAccept->item_id = $item->id;
-                $resourceSurvivorAccept->quantity = $quantityTrade;
-                $resourceSurvivorAccept->save();
+        try {
+            $survivorOffer = Survivor::find($survivor_offer_id);
+            if (!$survivorOffer) {
+                return response()->json([
+                    'message' => 'Survivor not found',
+                ], 404);
+            } elseif ($survivorOffer->infected == 'Y') {
+                return response()->json([
+                    'message' => 'Survivor infected!',
+                ], 400);
             }
-        }
 
-        foreach ($request->resourceSurvivorAccept as $itemName => $quantityTrade) {
-            $item = Item::where('name', 'like', '%' . $itemName . '%')->first();
-            $resourceSurvivorAccept = Resource::where('item_id','=',$item->id)->where('survivor_id','=',$survivor_accept_id)->first();
-            $resourceSurvivorAccept->quantity -= $quantityTrade;
-            $resourceSurvivorAccept->save();
-
-            $resourceSurvivorOffer = Resource::where('item_id','=',$item->id)->where('survivor_id','=',$survivor_offer_id)->first();
-            if(!empty($resourceSurvivorOffer)){
-                $resourceSurvivorOffer->quantity += $quantityTrade;
-                $resourceSurvivorOffer->save();
-            }else{
-                $resourceSurvivorOffer = new Resource();
-                $resourceSurvivorOffer->survivor_id = $survivor_offer_id;
-                $resourceSurvivorOffer->item_id = $item->id;
-                $resourceSurvivorOffer->quantity = $quantityTrade;
-                $resourceSurvivorOffer->save();
+            $survivorAccept = Survivor::find($survivor_accept_id);
+            if (!$survivorAccept) {
+                return response()->json([
+                    'message' => 'Survivor not found',
+                ], 404);
+            } elseif ($survivorAccept->infected == 'Y') {
+                return response()->json([
+                    'message' => 'Survivor infected!',
+                ], 400);
             }
-        }
 
-        return response()->json([
-            'message' => 'Trade done!',
-        ], 200);
-    }
+            $checkItemEquality = self::checkItemsEquality($request->resourceSurvivorOffer, $request->resourceSurvivorAccept);
+            if ($checkItemEquality == false) {
+                return response()->json([
+                    'message' => 'Different points!',
+                ], 400);
+            }
 
-    public function percentageInfected(){
-        $countAllSurvivors = Survivor::count();
-        $countInfetedSurvivors = Survivor::where('infected','Y')->count();
+            $checkQuantityMinSurvivorOffer = self::checkQuantityMin($request->resourceSurvivorOffer, $survivorOffer);
+            if ($checkQuantityMinSurvivorOffer == false) {
+                return response()->json([
+                    'message' => 'No resources available!',
+                ], 400);
+            }
 
-        $percentageInfected = (100 * $countInfetedSurvivors) / $countAllSurvivors;
+            $checkQuantityMinSurvivorAccept = self::checkQuantityMin($request->resourceSurvivorAccept, $survivorAccept);
+            if ($checkQuantityMinSurvivorAccept == false) {
+                return response()->json([
+                    'message' => 'No resources available!',
+                ], 400);
+            }
 
-        return response()->json([
-            'percentageInfected' => $percentageInfected,
-        ], 200);
-    }
+            foreach ($request->resourceSurvivorOffer as $itemName => $quantityTrade) {
+                $item = Item::where('name', 'like', '%' . $itemName . '%')->first();
+                $resourceSurvivorOffer = Resource::where('item_id', '=', $item->id)->where('survivor_id', '=', $survivor_offer_id)->first();
+                $resourceSurvivorOffer->quantity -= $quantityTrade;
+                $resourceSurvivorOffer->save();
 
-    public function percentageNonInfected(){
-        $countAllSurvivors = Survivor::count();
-        $countNonInfetedSurvivors = Survivor::where('infected','N')->count();
+                $resourceSurvivorAccept = Resource::where('item_id', '=', $item->id)->where('survivor_id', '=', $survivor_accept_id)->first();
 
-        $percentageNonInfected = (100 * $countNonInfetedSurvivors) / $countAllSurvivors;
+                if (!empty($resourceSurvivorAccept)) {
+                    $resourceSurvivorAccept->quantity += $quantityTrade;
 
-        return response()->json([
-            'percentageNonInfected' => $percentageNonInfected,
-        ], 200);
-    }
+                    $resourceSurvivorAccept->save();
+                } else {
+                    $resourceSurvivorAccept = new Resource();
+                    $resourceSurvivorAccept->survivor_id = $survivor_accept_id;
+                    $resourceSurvivorAccept->item_id = $item->id;
+                    $resourceSurvivorAccept->quantity = $quantityTrade;
+                    $resourceSurvivorAccept->save();
+                }
 
-    public function averageAmount(){
-        $countAllSurvivors = Survivor::count();
-        $allItems = Item::get();
-        $return = array();
-        foreach ($allItems as $item){
-            $countAllItem = Resource::where('item_id',$item->id)->sum('quantity');
+            }
 
-            $return[$item->name] = $countAllItem / $countAllSurvivors;
-        }
+            foreach ($request->resourceSurvivorAccept as $itemName => $quantityTrade) {
+                $item = Item::where('name', 'like', '%' . $itemName . '%')->first();
+                $resourceSurvivorAccept = Resource::where('item_id', '=', $item->id)->where('survivor_id', '=', $survivor_accept_id)->first();
+                $resourceSurvivorAccept->quantity -= $quantityTrade;
+                $resourceSurvivorAccept->save();
 
-        return response()->json([
-            $return
-        ], 200);
-    }
+                $resourceSurvivorOffer = Resource::where('item_id', '=', $item->id)->where('survivor_id', '=', $survivor_offer_id)->first();
+                if (!empty($resourceSurvivorOffer)) {
+                    $resourceSurvivorOffer->quantity += $quantityTrade;
+                    $resourceSurvivorOffer->save();
+                } else {
+                    $resourceSurvivorOffer = new Resource();
+                    $resourceSurvivorOffer->survivor_id = $survivor_offer_id;
+                    $resourceSurvivorOffer->item_id = $item->id;
+                    $resourceSurvivorOffer->quantity = $quantityTrade;
+                    $resourceSurvivorOffer->save();
+                }
+            }
 
-    public function pointsLost($survivor_infected_id){
-        $survivorInfected = Survivor::find($survivor_infected_id);
-        if (!$survivorInfected) {
             return response()->json([
-                'message' => 'Survivor not found',
-            ], 404);
+                'message' => 'Trade done!',
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
         }
+    }
 
-        if($survivorInfected->infected == 'N'){
+    public function percentageInfected()
+    {
+        try {
+            $countAllSurvivors = Survivor::count();
+            $countInfetedSurvivors = Survivor::where('infected', 'Y')->count();
+
+            $percentageInfected = (100 * $countInfetedSurvivors) / $countAllSurvivors;
+
             return response()->json([
-                'message' => 'Survivor not infected, yet...',
-            ], 400);
+                'percentageInfected' => $percentageInfected,
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
         }
+    }
 
-        $countAllItems = Resource::where('survivor_id',$survivor_infected_id)->sum('quantity');
-        return response()->json([
-            'pointsLost' => $countAllItems,
-        ], 200);
+    public function percentageNonInfected()
+    {
+        try {
+            $countAllSurvivors = Survivor::count();
+            $countNonInfetedSurvivors = Survivor::where('infected', 'N')->count();
+
+            $percentageNonInfected = (100 * $countNonInfetedSurvivors) / $countAllSurvivors;
+
+            return response()->json([
+                'percentageNonInfected' => $percentageNonInfected,
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
+        }
+    }
+
+    public function averageAmount()
+    {
+        try {
+            $countAllSurvivors = Survivor::count();
+            $allItems = Item::get();
+            $return = array();
+            foreach ($allItems as $item) {
+                $countAllItem = Resource::where('item_id', $item->id)->sum('quantity');
+
+                $return[$item->name] = $countAllItem / $countAllSurvivors;
+            }
+
+            return response()->json([
+                $return
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
+        }
+    }
+
+    public function pointsLost($survivor_infected_id)
+    {
+        try {
+            $survivorInfected = Survivor::find($survivor_infected_id);
+            if (!$survivorInfected) {
+                return response()->json([
+                    'message' => 'Survivor not found',
+                ], 404);
+            }
+
+            if ($survivorInfected->infected == 'N') {
+                return response()->json([
+                    'message' => 'Survivor not infected, yet...',
+                ], 400);
+            }
+
+            $countAllItems = Resource::where('survivor_id', $survivor_infected_id)->sum('quantity');
+            return response()->json([
+                'pointsLost' => $countAllItems,
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(["error" => $exception->getMessage()]);
+        }
     }
 
     private static function checkItemsEquality($resourceSurvivorOffer, $resourceSurvivorAccept)
